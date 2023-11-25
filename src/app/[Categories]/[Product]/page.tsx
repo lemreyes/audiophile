5176e54b068b6e3abf310f8f5afd41b77a106178
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-11-25 14:06:08 +0800

    product detail query and error, notfound pages.

--- src/app/[Categories]/[Product]/page.tsx
+++ src/app/[Categories]/[Product]/page.tsx
@@ -1,7 +1,33 @@
-export default function ProductDetail(params: { searchParams: any }) {
-  console.log("params", params);
+import { notFound } from "next/navigation";
+import prisma from "../../Utilities/prismaUtils";
 
-  console.log("searchParams", params.searchParams);
+type Props = {
+  params: {};
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-  return <div>Product Detail</div>;
+export default async function ProductDetail(params: Props) {
+  const productId = parseInt(params.searchParams.id as string);
+  if (productId <= 0) {
+    throw new Error("Invalid product Id.");
+  }
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id: productId,
+    },
+    include: {
+      image: true,
+      categoryImage: true,
+      gallery: true,
+      inclusions: true,
+      otherProducts: true,
+      imageAsOther: true,
+    },
+  });
+  if (!product) {
+    notFound();
+  }
+
+  return <main>Product Detail</main>;
 }
