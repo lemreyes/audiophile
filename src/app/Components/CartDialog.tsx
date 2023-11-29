70dfb7dfe0a2455b691a7cd0213b18a65c3c8270
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-11-29 22:49:37 +0800

    Cart dialog placement

---
+++ src/app/Components/CartDialog.tsx
@@ -0,0 +1,18 @@
+export default function CartDialog({
+  hdlClickOutside,
+}: {
+  hdlClickOutside: () => void;
+}) {
+  return (
+    <div
+      className="w-screen h-screen bg-black bg-opacity-40 absolute z-50 top-0 left-0 flex flex-col items-center"
+      onClick={hdlClickOutside}
+    >
+      <div className="w-11/12 desktop:w-4/5 wide:w-3/5 mt-12 desktop:mt-24 flex flex-col items-end">
+        <div className="bg-white rounded-lg w-full tablet:w-7/12">
+          <h3>Cart</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,41 +1,65 @@
+"use client";
 import Image from "next/image";
 import hamburger_icon from "../../../public/assets/shared/tablet/icon-hamburger.svg";
 import cart_icon from "../../../public/assets/shared/desktop/icon-cart.svg";
 import logo from "../../../public/assets/shared/desktop/logo.svg";
 import Link from "next/link";
+import { useState, useEffect, useRef, Fragment } from "react";
+import CartDialog from "./CartDialog";
 
 export default function Navbar() {
+  const [isShowCartDialog, setIsShowCartDialog] = useState(false);
+
+  const toggleCartDialog = () => {
+    console.log("toggle cart dialog");
+    setIsShowCartDialog(!isShowCartDialog);
+  };
+
+  const hdlClickOutside = () => {
+    setIsShowCartDialog(false);
+  };
+
+  console.log("isShowCartDialog", isShowCartDialog);
+
   return (
-    <header className="bg-primary py-4 flex flex-col items-center ">
-      <nav className="w-10/12 tablet:w-11/12 desktop:w-4/5 wide:w-3/5 flex flex-row justify-between items-center">
-        <Image
-          src={hamburger_icon}
-          alt="burger_icon"
-          className="w-4 h-4 desktop:hidden"
-          width={40}
-          style={{
-            height: "auto",
-          }}
-        />
-        <Link href="/">
-          <Image src={logo} alt="logo" />
-        </Link>
-        <ul className="hidden desktop:flex desktop:flex-row desktop:gap-x-8 text-white uppercase text-[13px] tracking-[2px] text-center">
+    <Fragment>
+      <header className="bg-primary py-4 flex flex-col items-center ">
+        <nav className="w-10/12 tablet:w-11/12 desktop:w-4/5 wide:w-3/5 flex flex-row justify-between items-center">
+          <Image
+            src={hamburger_icon}
+            alt="burger_icon"
+            className="w-4 h-4 desktop:hidden"
+            width={40}
+            style={{
+              height: "auto",
+            }}
+          />
           <Link href="/">
-            <li className="my-8 hover:text-accent">Home</li>
+            <Image src={logo} alt="logo" />
           </Link>
-          <Link href="/Headphones">
-            <li className="my-8 hover:text-accent">Headphones</li>
-          </Link>
-          <Link href="/Speakers">
-            <li className="my-8 hover:text-accent">Speakers</li>
-          </Link>
-          <Link href="/Earphones">
-            <li className="my-8 hover:text-accent">Earphones</li>
-          </Link>
-        </ul>
-        <Image src={cart_icon} alt="cart_icon" className="w-4 h-4" />
-      </nav>
-    </header>
+          <ul className="hidden desktop:flex desktop:flex-row desktop:gap-x-8 text-white uppercase text-[13px] tracking-[2px] text-center">
+            <Link href="/">
+              <li className="my-8 hover:text-accent">Home</li>
+            </Link>
+            <Link href="/Headphones">
+              <li className="my-8 hover:text-accent">Headphones</li>
+            </Link>
+            <Link href="/Speakers">
+              <li className="my-8 hover:text-accent">Speakers</li>
+            </Link>
+            <Link href="/Earphones">
+              <li className="my-8 hover:text-accent">Earphones</li>
+            </Link>
+          </ul>
+          <Image
+            src={cart_icon}
+            alt="cart_icon"
+            className="w-4 h-4"
+            onClick={toggleCartDialog}
+          />
+        </nav>
+      </header>
+      {isShowCartDialog && <CartDialog hdlClickOutside={hdlClickOutside} />}
+    </Fragment>
   );
 }
