9c05da02f974f60870142bbdabe70aa100c5b96a
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-12-03 21:26:05 +0800

    Increment, decrement product quantity

--- src/app/[Categories]/[Product]/Components/AddToCart.tsx
+++ src/app/[Categories]/[Product]/Components/AddToCart.tsx
@@ -1,10 +1,13 @@
+"use client";
+import { useState } from "react";
 import CountButton from "./CountButton";
 
 export default function AddToCart() {
+  const [quantity, setQuantity] = useState(1);
   return (
     <div className="flex flex-row items-center gap-x-4">
       <div className="flex flex-row items-center">
-        <CountButton />
+        <CountButton quantity={quantity} btnHandler={setQuantity} />
       </div>
       <button
         className="py-4 px-8 uppercase text-[13px] font-bold tracking-wide text-white bg-accent 
--- src/app/[Categories]/[Product]/Components/CountButton.tsx
+++ src/app/[Categories]/[Product]/Components/CountButton.tsx
@@ -1,17 +1,37 @@
-"use client";
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 
-export default function CountButton() {
-  const [quantity, setQuantity] = useState(1);
+export default function CountButton({
+  quantity,
+  btnHandler,
+}: {
+  quantity: number;
+  btnHandler: Dispatch<SetStateAction<number>>;
+}) {
+  const hdlIncrementButton = () => {
+    btnHandler(quantity + 1);
+  };
+
+  const hdlDecrementButton = () => {
+    if (quantity > 1) {
+      btnHandler(quantity - 1);
+    }
+  };
+
   return (
-    <div className="flex flex-row items-center">
-      <button className="text-[13px] font-bold tracking-wide bg-product p-4 text-textPrimary">
+    <div className="flex flex-row items-center justify-between">
+      <button
+        className="text-[13px] font-bold tracking-wide bg-product p-4 text-textPrimary hover:text-accent"
+        onClick={hdlDecrementButton}
+      >
         -
       </button>
-      <p className="text-[13px] font-bold tracking-wide bg-product p-4">
+      <p className="text-[13px] font-bold tracking-wide bg-product p-4 w-8">
         {quantity}
       </p>
-      <button className="text-[13px] font-bold tracking-wide bg-product p-4 text-textPrimary">
+      <button
+        className="text-[13px] font-bold tracking-wide bg-product p-4 text-textPrimary hover:text-accent"
+        onClick={hdlIncrementButton}
+      >
         +
       </button>
     </div>
