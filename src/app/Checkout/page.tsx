f79f7b20ca1ac8400f6276ac1d3e7fd658779f7a
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-12-10 19:52:12 +0800

    Checkout page

---
+++ src/app/Checkout/page.tsx
@@ -0,0 +1,17 @@
+export default function CheckoutPage() {
+  return (
+    <div>
+      <div>
+        <span>Go Back</span>
+      </div>
+      <main>
+        <section>
+          <h1>Checkout</h1>
+        </section>
+        <aside>
+          <h2>Summary</h2>
+        </aside>
+      </main>
+    </div>
+  );
+}
