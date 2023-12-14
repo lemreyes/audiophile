27d34b5d5431c4bc158d5f98d40cca4ea4dfb770
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-12-14 23:42:15 +0800

    Checkout tablet layout

--- src/app/Checkout/page.tsx
+++ src/app/Checkout/page.tsx
@@ -37,146 +37,199 @@ export default function CheckoutPage() {
               <h2 className="text-accent text-[13px] font-bold tracking-wide leading-[25px] mt-8 uppercase">
                 Billing Details
               </h2>
-              <label
-                htmlFor="name"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="Alexei Ward"
-              />
-              <label
-                htmlFor="email"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Email Address
-              </label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="alexei@mail.com"
-              />
-              <label
-                htmlFor="number"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="number"
-                id="number"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="+1 202-555-0136"
-              />
+              <div className="tablet:grid tablet:grid-cols-2 tablet:grid-rows-2 tablet:gap-x-4">
+                <div className="tablet:col-start-1 row-start-1">
+                  <label
+                    htmlFor="name"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="Alexei Ward"
+                  />
+                </div>
+                <div className="tablet:col-start-2 tablet:row-start-1">
+                  <label
+                    htmlFor="email"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="alexei@mail.com"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="number"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    name="number"
+                    id="number"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="+1 202-555-0136"
+                  />
+                </div>
+              </div>
             </div>
             <div>
               <h2 className="text-accent text-[13px] font-bold tracking-wide leading-[25px] mt-8 uppercase">
                 Shipping Info
               </h2>
-              <label
-                htmlFor="address"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Your Address
-              </label>
-              <input
-                type="text"
-                name="address"
-                id="address"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="1137 Williams Avenue"
-              />
-              <label
-                htmlFor="zip"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                ZIP Code
-              </label>
-              <input
-                type="text"
-                name="zip"
-                id="zip"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="10001"
-              />
-              <label
-                htmlFor="city"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                City
-              </label>
-              <input
-                type="text"
-                name="city"
-                id="city"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="New York"
-              />
-              <label
-                htmlFor="country"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Country
-              </label>
-              <input
-                type="text"
-                name="country"
-                id="country"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="United States"
-              />
+              <div className="tablet:grid tablet:grid-cols-2 tablet:grid-rows-3 tablet:gap-x-4">
+                <div className="tablet:col-span-2">
+                  <label
+                    htmlFor="address"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    Your Address
+                  </label>
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="1137 Williams Avenue"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="zip"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    ZIP Code
+                  </label>
+                  <input
+                    type="text"
+                    name="zip"
+                    id="zip"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="10001"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="city"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="New York"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="country"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    Country
+                  </label>
+                  <input
+                    type="text"
+                    name="country"
+                    id="country"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="United States"
+                  />
+                </div>
+              </div>
             </div>
             <div>
               <h2 className="text-accent text-[13px] font-bold tracking-wide leading-[25px] mt-8 uppercase">
                 Payment Details
               </h2>
-              <label
-                htmlFor="paymentMethod"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                Payment Method
-              </label>
-              <input
-                type="radio"
-                name="paymentMethod"
-                id="paymentMethod"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="1137 Williams Avenue"
-              />
-              <label
-                htmlFor="emoneyNumber"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                e-Money Number
-              </label>
-              <input
-                type="text"
-                name="emoneyNumber"
-                id="emoneyNumber"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="238521993"
-              />
-              <label
-                htmlFor="emoneyPin"
-                className="mt-4 text-[12px] font-bold tracking-tight"
-              >
-                e-Money Pin
-              </label>
-              <input
-                type="text"
-                name="emoneyPin"
-                id="emoneyPin"
-                className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
-                placeholder="6891"
-              />
+              <div className="tablet:grid tablet:grid-cols-2 tablet:grid-rows-2 tablet:gap-4">
+                <div className="tablet:col-span-2">
+                  <div className="tablet:grid tablet:grid-cols-2">
+                    <label
+                      htmlFor=""
+                      className="mt-4 text-[12px] font-bold tracking-tight tablet:row-span-2"
+                    >
+                      Payment Method
+                    </label>
+                    <div className="mt-2 flex flex-row items-center gap-x-4 border border-gray-400 rounded-lg px-4 py-1">
+                      <input
+                        type="radio"
+                        name="paymentMethod"
+                        id="e-Money"
+                        className="mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                        placeholder=""
+                        value="e-Money"
+                      />
+                      <label
+                        htmlFor="e-Money"
+                        className="text-[14px] font-bold tracking-tight"
+                      >
+                        e-Money
+                      </label>
+                    </div>
+                    <div className="mt-4 flex flex-row items-center gap-x-4 border border-gray-400 rounded-lg px-4 py-1">
+                      <input
+                        type="radio"
+                        name="paymentMethod"
+                        id="cod"
+                        className="mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                        placeholder=""
+                        value="cod"
+                      />
+                      <label
+                        htmlFor="cod"
+                        className="text-[14px] font-bold tracking-tight"
+                      >
+                        Cash on Delivery
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-8 tablet:mt-0 tablet:col-start-1 tablet:row-start-2">
+                  <label
+                    htmlFor="emoneyNumber"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    e-Money Number
+                  </label>
+                  <input
+                    type="text"
+                    name="emoneyNumber"
+                    id="emoneyNumber"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="238521993"
+                  />
+                </div>
+                <div className="tablet:col-start-2 tablet:row-start-2">
+                  <label
+                    htmlFor="emoneyPin"
+                    className="mt-4 text-[12px] font-bold tracking-tight"
+                  >
+                    e-Money Pin
+                  </label>
+                  <input
+                    type="text"
+                    name="emoneyPin"
+                    id="emoneyPin"
+                    className="block w-full mt-4 mb-4 p-4 rounded-lg border border-gray-400 text-14px font-bold tracking-tight"
+                    placeholder="6891"
+                  />
+                </div>
+              </div>
             </div>
           </form>
         </section>
