a0bb3d81ae384b79aa5850b4a2200c258c0ad894
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-12-26 09:06:38 +0800

    interfaces for customer, transaction, order info

--- src/app/Types/Enums.ts
+++ src/app/Types/Enums.ts
@@ -2,3 +2,9 @@ export enum PAYMENT_METHOD {
   EMONEY = 0,
   COD,
 }
+
+export enum ScreenTypeEnum {
+  SCREENTYPE_MOBILE = 0,
+  SCREENTYPE_TABLET,
+  SCREENTYPE_DESKTOP,
+}
--- src/app/Types/Interfaces.ts
+++ src/app/Types/Interfaces.ts
@@ -1,8 +1,4 @@
-export enum ScreenTypeEnum {
-  SCREENTYPE_MOBILE = 0,
-  SCREENTYPE_TABLET,
-  SCREENTYPE_DESKTOP,
-}
+import { PAYMENT_METHOD } from "./Enums";
 
 export interface ProductInfo {
   id: number;
@@ -37,4 +33,31 @@ export interface GalleryInfo {
   productId: number;
   created_at: Date;
   updated_at: Date;
+}
+
+export interface CustomerInfo {
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  zipcode: string;
+  city: string;
+  country: string;
+}
+
+export interface TransactionInfo {
+  id?: number;
+  paymentMethod: PAYMENT_METHOD;
+  eMoneyNumber: string;
+  eMoneyPin?: string;
+  shippingFee: number;
+  vatRate: number;
+  orders: Array<OrderItem>;
+}
+
+export interface OrderItem {
+  id?: number;
+  productId: number;
+  quantity: number;
 }
