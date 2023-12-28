b3a7c30a498f19558ab3857860c07c2bc13403c9
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-12-28 21:55:52 +0800

    pass data to transactions api route

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -3,5 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   console.log("POST /api/transactions");
 
+  // get data
+  const { customerInfo, transactionInfo } = await request.json();
+
+  console.log("customerInfo ", customerInfo);
+  console.log("transactionInfo", transactionInfo);
+
+  // validate
+
+  // store customer info if not yet existing
+
+  // store transaction
+
   return NextResponse.json({ status: "OK" });
 }
