ba5d4148bdcdf8af296676e512d793599c356fb5
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-02 10:15:42 +0800

    Update layouts for notfound and error pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 "use client"; // Error components must be Client Components
 
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Error({
   error,
@@ -9,22 +10,42 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <main className="flex items-center justify-center">
+      <div className="w-10/12 tablet:w-11/12 desktop:w-4/5 wide:w-3/5 mt-8 mb-8 border border-accent p-8">
+        <h2 className="text-[32px] text-accent font-bold leading-[36px] tracking-wide">
+          Sorry, something went wrong!
+        </h2>
+        <p className="mb-8 text-[15px] text-gray-400 font-medium leading-[25px]">
+          {error.message}
+        </p>
+        <button
+          className="w-full mt-8 py-4 uppercase text-white text-[13px] font-bold tracking-[1px] bg-accent hover:bg-accentHover
+                    disabled:bg-gray-400"
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+        <button
+          className="w-full mt-8 py-4 uppercase text-white text-[13px] font-bold tracking-[1px] bg-accent hover:bg-accentHover
+                    disabled:bg-gray-400"
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          Back to home
+        </button>
+      </div>
+    </main>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,31 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="flex items-center justify-center">
+      <div className="w-10/12 tablet:w-11/12 desktop:w-4/5 wide:w-3/5 mt-8 mb-8 border border-accent p-8">
+        <h2 className="text-[32px] text-accent font-bold leading-[36px] tracking-wide">
+          Not Found
+        </h2>
+        <p className="mb-8 text-[15px] text-gray-400 font-medium leading-[25px]">
+          Could not find requested resource
+        </p>
+        <button
+          className="w-full mt-8 py-4 uppercase text-white text-[13px] font-bold tracking-[1px] bg-accent hover:bg-accentHover
+                          disabled:bg-gray-400"
+          onClick={() => {
+            // restore scrolling
+            document.body.style.overflowY = "auto";
+            document.body.style.overflowX = "auto";
+            router.push("/");
+          }}
+        >
+          Back to home
+        </button>
+      </div>
+    </main>
   );
 }
